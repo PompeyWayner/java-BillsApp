aed81a17d9032e58f73300c360d547826edcfe30
Wayne Sandford <wsandford@hotmail.com>
2019-07-27 19:08:36 +0100

    MainWindow UI update

--- src/bills/Main.java
+++ src/bills/Main.java
@@ -6,6 +6,12 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+/**
+ * Main class
+ *
+ * @author Wayne Sandford
+ * @version 16-07-2019
+ */
 public class Main extends Application {
 
     @Override
